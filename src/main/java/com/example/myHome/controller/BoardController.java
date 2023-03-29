296e937cfba9fb17c58d73233492cb9867082183
SSupRoot <thought3860@naver.com>
2023-03-30 00:50:46 +0900

    myFirst Commit

---
+++ src/main/java/com/example/myHome/controller/BoardController.java
@@ -0,0 +1,25 @@
+package com.example.myHome.controller;
+
+import com.example.myHome.model.Board;
+import com.example.myHome.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards", boards);
+        return "board/list";
+    }
+}
